2457b66f5fdd7416dd9ab6361272013ddebccf34
Murillo de Miranda Pereira <murillomir0@gmail.com>
2025-12-30 14:06:38 -0300

    feat: setup app

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "./globals.css";
+
+import type { Metadata } from "next";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Rick and Morty Dashboard",
+  description: "A dashboard for the Rick and Morty API",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
